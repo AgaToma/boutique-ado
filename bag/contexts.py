397b6a9193dff932aa8d1fde0fe198ef85530eac
AgaToma <aga.tomashevska@gmail.com>
2023-08-02 19:54:01 +0000

    add size logic to bag view and contexts

--- bag/contexts.py
+++ bag/contexts.py
@@ -10,15 +10,31 @@ def bag_contents(request):
     product_count = 0
     bag = request.session.get('bag', {})
 
-    for item_id, quantity in bag.items():
-        product = get_object_or_404(Product, pk=item_id)
-        total += quantity * product.price
-        product_count += quantity
-        bag_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'product': product,
-        })
+    # item_data represents 2 types of quantity data, for items without sizes item_data is just quantity as integer
+    # for items with sizes it's a dictionary of all items by size
+    for item_id, item_data in bag.items():
+        # checking if item_data (quantity) is integer, so it's a no size item
+        if isinstance(item_data, int):
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * product.price
+            product_count += quantity
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+            })
+        else:
+            product = get_object_or_404(Product, pk=item_id)
+            # for sized items we need to iterate through a dictionary of items by size
+            for size, quantity in item_data['items_by_size'].items():
+                total += quantity * product.price
+                product_count += quantity
+                bag_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+                'size': size,
+            })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
--- bag/views.py
+++ bag/views.py
@@ -14,11 +14,25 @@ def add_to_bag(request, item_id):
     # obtain quantity from product detail template form, change to integer as it comes as string
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
     # access the request's session, get variable bag if already exists, if not initialize to an empty dictionary
     # this is to store bag for the duration of the session
     bag = request.session.get('bag', {})
-    # add products and quantity to the created dictionary
 
+    # add products and quantity to the created dictionary after checking if there was size and if
+    # item with this size was already there
+    if size:
+        if item_id in list(bag.keys()):
+            if size in bag[item_id]['items_by_size'].keys():
+                bag[item_id]['items_by_size'][size] += quantity
+            else:
+                bag[item_id] = ['items_by_size'][size] += quantity
+        else:
+            bag[item_id] = {'items_by_size': {size: quantity}}
+    else:
+    # add products and quantity to the created dictionary
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
     else:
