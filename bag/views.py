dc99fb7fd2741304f5588a680072a09a79a21608
AgaToma <aga.tomashevska@gmail.com>
2023-08-04 14:21:21 +0000

    set up toasts

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+from products.models import Product
 
 # Create your views here.
 
@@ -11,6 +13,7 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """Add a quantity of the specified product to the shopping bag"""
 
+    product = Product.objects.get(pk=item_id)
     # obtain quantity from product detail template form, change to integer as it comes as string
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
@@ -37,6 +40,7 @@ def add_to_bag(request, item_id):
             bag[item_id] += quantity
         else:
             bag[item_id] = quantity
+            messages.success(request, f'Added {product.name} to your bag')
     # overriting the variable bag in the session with its updated version
     request.session['bag'] = bag
     return redirect(redirect_url)
