fe5af7edf399ceba45a3d6083de86b7e4e159d21
AgaToma <aga.tomashevska@gmail.com>
2023-08-05 16:11:47 +0000

    add stripe

--- checkout/views.py
+++ checkout/views.py
@@ -13,7 +13,9 @@ def checkout(request):
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
-        'order_form': order_form
+        'order_form': order_form,
+        'stripe_public_key':'pk_test_51NbmeUBFg1Re1y2TIXgexFh7ZPP8oZA4PVpsv1JWUtWGnhKEKz7cgwc9N9AATl7yTi4svfBfBzHZAnxdQBgMcMxd00fcY8n2mN',
+        'client_secret':'test client secret',
     }
 
     return render(request, template, context)
