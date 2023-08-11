74704f0d7f25da04f6ce0fc9d5e8300c67e71648
AgaToma <aga.tomashevska@gmail.com>
2023-08-11 14:05:22 +0000

    Add order history to user profile

--- profiles/views.py
+++ profiles/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 
 from .models import UserProfile
 from .forms import UserProfileForm
 
+from checkout.models import Order
 
 def profile(request):
     """Display the user profile"""
 
     profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully')
+
 
     form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
@@ -16,6 +25,26 @@ def profile(request):
     context = {
         'form': form,
         'orders': orders,
+        # this is to stop showing bag in profile update success message, links to toast_success template
+        'on_profile_page': True,
     }
 
     return render(request, template, context)
+
+
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    message.info(request, (
+        f'This is a past confirmation for order number {order_number}.'
+        'A confirmation email was sent on the order date'
+    ))
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
+    }
+
+    return render(request, template, context)
+
